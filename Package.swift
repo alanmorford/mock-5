75e2512a25a1b3c4c1d0a15a4c0c1e03c882c5e5
alanmorford <igginz@gmail.com>
2023-09-01 10:55:47 -0700

    Add resources config to target

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "mock-5",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "mock-5Tests",
             dependencies: ["mock-5"]),
